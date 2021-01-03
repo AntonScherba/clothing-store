f43912c8d4470b23439be65f94117d1ea2ab3128
AntonScherba <antonscherba95@gmail.com>
2021-01-03 20:00:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+import HomePage from "./pages/HomePage/HomePage";
+import ShopPage from "./pages/Shop/ShopPage";
+import SignInAndSignUp from "./pages/SignInAndSignUp/SignInAndSignUp";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
+      </Switch>
     </div>
   );
 }
